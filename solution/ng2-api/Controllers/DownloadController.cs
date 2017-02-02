e767f0c12e447436ffe8ef914ca4ac40cce8b733
Eros Niko Alvarez <iamerosniko@yahoo.com>
2017-02-02 11:11:56 -0500

    created download controller

---
+++ solution/ng2-api/Controllers/DownloadController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.IO;
+using System.Net.Http.Headers;
+using System.Web;
+
+namespace ng2_api.Controllers
+{
+    public class DownloadController : ApiController
+    {
+        // GET api/download
+        public HttpResponseMessage Get(string filename)
+        {
+            //server's shared folder / upload repo
+            var path = HttpContext.Current.Server.MapPath("~/upload");
+
+            HttpResponseMessage result = new HttpResponseMessage(HttpStatusCode.OK);
+            //reads the path and the file
+            try {
+                var stream = new FileStream(path + "/" + @filename, FileMode.Open);
+                result.Content = new StreamContent(stream);
+                result.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                result.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment");
+                //@filename = original name inside the repo
+                result.Content.Headers.ContentDisposition.FileName = @filename;
+                return result;
+            }
+            catch
+            {
+                return new HttpResponseMessage(HttpStatusCode.NotFound);
+            }
+        }
+    }
+}
